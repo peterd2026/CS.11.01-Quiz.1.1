da3c8e041d27706fcd92923816049f4a65196be6
peterdeng <peterd2026@student.cis.edu.hk>
2023-09-28 13:38:04 +0800

    quiz 1.1 ide

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 import java.util.Scanner;
+/*I have neither given nor received unauthorized aid on these pieces of work*/
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(system.in)
+
+        int age;
+
+        String firstName;
+
+        String favouriteFood;
+
+        System.out.println("please enter your age");
+
+        System.out.println("please enter your age");
+            int age = scanner.nextInt();
+
+        System.out.println("please enter your first name");
+                int firstName = scanner.nextInt();
+
+        System.out.println("please enter your favourite food");
+                int favouriteFood = scanner.nextInt();
+
+        int age = 18;
+        String firstName = "清清"；
+        String favouriteFood = "火锅"；
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Age: " + age);
+        System.lut.println("Favourite food: " + favouriteFood);
+
+
 
 
 
